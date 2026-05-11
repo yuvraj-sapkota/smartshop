b29678f9d57882d7dd98d6e386cac46b006257e3
yuvraj-sapkota <sharmayuvraj034@gmail.com>
2026-05-11 06:56:55 +0545

    create the seller register

--- frontend/src/auth/signup/SellerSignup.jsx
+++ frontend/src/auth/signup/SellerSignup.jsx
@@ -1,7 +1,135 @@
-import React from "react";
+import React, { useState } from "react";
+import AuthForm from "../AuthForm";
 
 const SellerSignup = () => {
-  return <div className="text-xl flex items-center justify-center h-screen">SellerSignup is coming soon</div>;
+  const [form, setForm] = useState({
+    referBy: "",
+    username: "",
+    email: "",
+    password: "",
+    storename: "",
+    storeaddress: "",
+  });
+  const [errors, setErrors] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+
+    setErrors({
+      ...errors,
+      [e.target.name]: "",
+    });
+  };
+
+  const validate = () => {
+    let newErrors = {};
+
+    if (!form.username.trim()) {
+      newErrors.username = "Username is required";
+    }
+
+    if (!form.email.trim()) {
+      newErrors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(form.email)) {
+      newErrors.email = "Invalid email";
+    }
+
+    if (!form.password.trim()) {
+      newErrors.password = "Password is required";
+    } else if (form.password.length < 6) {
+      newErrors.password = "Minimum 6 characters required";
+    }
+
+    if (!form.storename.trim()) {
+      newErrors.storename = "Store name is required";
+    }
+    if (!form.storeaddress.trim()) {
+      newErrors.storeaddress = "Store address is required";
+    }
+
+    setErrors(newErrors);
+
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!validate()) return;
+
+    try {
+      setIsLoading(true);
+
+      console.log(form);
+
+      // API CALL HERE
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const signupFields = [
+    {
+      label: "Referral Username",
+      name: "referBy",
+      type: "text",
+      placeholder: "Referral code if any",
+    },
+    {
+      label: "Username",
+      name: "username",
+      type: "text",
+      placeholder: "Enter username",
+    },
+    {
+      label: "Email",
+      name: "email",
+      type: "email",
+      placeholder: "Enter email",
+    },
+    {
+      label: "Password",
+      name: "password",
+      type: "password",
+      placeholder: "Create password",
+    },
+    {
+      label: "Store Name",
+      name: "storename",
+      type: "text",
+      placeholder: "Enter storeName",
+    },
+    {
+      label: "Store Address",
+      name: "storeaddress",
+      type: "textarea",
+      placeholder: "Enter your store address",
+    },
+  ];
+  return (
+    <>
+      <AuthForm
+        title="Create Seller Account"
+        subtitle="Create your seller account to continue"
+        fields={signupFields}
+        form={form}
+        errors={errors}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        buttonText="Create Account"
+        footerText="Already have an account?"
+        footerLinkText="Login"
+        footerLink="/login"
+        isLoading={isLoading}
+      />
+    </>
+  );
 };
 
 export default SellerSignup;
