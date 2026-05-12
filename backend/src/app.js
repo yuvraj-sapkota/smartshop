c4f9b152bfd1569bf0521f290642427c6ff1df42
yuvraj-sapkota <sharmayuvraj034@gmail.com>
2026-05-12 09:39:28 +0545

    origin issue solved

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,9 +8,20 @@ dotenv.config();
 
 const app = express();
 
+const allowedOrigins = [
+  "http://192.168.1.13:5173",
+  "https://smartshop.vercel.app",
+];
+
 app.use(
   cors({
-    origin: "http://192.168.1.13:5173",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   }),
 );
